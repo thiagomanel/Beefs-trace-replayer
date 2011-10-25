4963231f320819c33ecfe2e2921291e56ed87501
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2011-10-25 18:11:32 -0300

    replayer_test bug solved, i missed 'python' keyword

--- tests/replay_input/replayer_test.sh
+++ tests/replay_input/replayer_test.sh
@@ -5,4 +5,4 @@ workload_input=$2
 
 
 strace $replayer_path $workload_input 2> strace.output
-./match_syscalls.py strace.output $workload_input
+python match_syscalls.py strace.output $workload_input

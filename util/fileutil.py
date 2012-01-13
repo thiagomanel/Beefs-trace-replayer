3732bd55858257c63ad780585f2d747d81cf369f
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2012-01-13 17:42:15 -0300

    going home

--- util/fileutil.py
+++ util/fileutil.py
@@ -1,33 +1,44 @@
 from enum import Enum
 
-access_modes = Enum("O_RDONLY", "O_WRONLY", "O_RDWR")
+#		   (oct ?)  dec
+#O_ACCMODE         0003	      	 3			
+#O_RDONLY            00		 0
+#O_WRONLY            01		 1
+#O_RDWR              02		 2
 
-# something outside the range [0,3] is a programming error and the code should fail miserable
-__value2access_modes__ = {0:access_modes.O_RDONLY, 1:access_modes.O_WRONLY, 2:access_modes.O_RDWR, 3:None}
+#O_CREAT           0100		64
+#O_EXCL            0200	       128
+#O_NOCTTY          0400	       256
+#O_TRUNC          01000        512
 
-#O_ACCMODE	   0003
-#O_RDONLY	     00
-#O_WRONLY	     01
-#O_RDWR		     02
-
-#O_CREAT	   0100
-#O_EXCL		   0200
-#O_NOCTTY	   0400
-#O_TRUNC	  01000
-#O_APPEND	  02000
-#O_NONBLOCK	  04000
+#O_APPEND         02000	      1024
+#O_NONBLOCK       04000	      2048
 #O_NDELAY    O_NONBLOCK
-#O_SYNC		 010000
-#O_FSYNC	 O_SYNC
-#O_ASYNC	 020000
+#O_SYNC          010000       4096
+#O_FSYNC         O_SYNC
+#O_ASYNC         020000       8192
 
-""" 
-   conversion from linux open_mode to its string representation.
-   see http://linux.die.net/man/2/open
+ACCESS_MODES = Enum("O_RDONLY", "O_WRONLY", "O_RDWR")
+CREATION_FLAGS = Enum("O_CREAT", "O_EXCL", "O_NOCTTY", "O_TRUNC")
+
+# something outside the range [0,3] is a programming error and the code should 
+# fail
+__value2access_modes__ = {0:ACCESS_MODES.O_RDONLY, 1:ACCESS_MODES.O_WRONLY, 
+                           2:ACCESS_MODES.O_RDWR, 3:None}
+
+__value2creation_flags__ = {8:CREATION_FLAGS.O_TRUNC}
+
+""" from linux open flags to a human-friendly representation. 
+http://linux.die.net/man/2/open
 """
-def access_mode_bits(flags):
-    return flags & 3
+def access_mode_bits(open_flags):
+    return open_flags & 3
 
-def access_mode(flags):
-#what if a value that does not match ?
-    return __value2access_modes__[access_mode_bits(flags)]
+def access_mode(open_flags):
+    return __value2access_modes__[access_mode_bits(open_flags)]
+
+def creation_flags_bits(open_flags):
+    return open_flags & ~3
+
+def creation_flags(open_flags):
+    return __value2creation_flags__[creation_flags_bits(open_flags)]
--- util/fileutil_unittest.py
+++ util/fileutil_unittest.py
@@ -1,28 +1,47 @@
 import unittest
 from enum import Enum
 from fileutil import access_mode
-from fileutil import access_modes
+from fileutil import creation_flags
+from fileutil import ACCESS_MODES
+from fileutil import CREATION_FLAGS
 
 class TestFileUtil(unittest.TestCase):
 
     """ an acceptance test """
-    def test_acceptance_access_mode_34816(self):
-	#we need an explanation about 34816 meaning
+    def test_access_mode_34816(self):
+	#(O_RDONLY|O_NONBLOCK|O_LARGEFILE)=34816
         mode = access_mode(34816)
-	
-	#order is not important
-        self.assertEqual(mode, access_modes.O_RDONLY)
+        self.assertEqual(mode, ACCESS_MODES.O_RDONLY)
 
-    def test_acceptance_mode_33281(self):
+    def test_creation_flags_34816(self):
+	#(O_RDONLY|O_NONBLOCK|O_LARGEFILE)=34816
+        c_flags = creation_flags(33816)
+	#order is not important
+        self.assertSequenceEqual(c_flags, [CREATION_FLAGS.O_TRUNC])
+
+    def test_access_mode_33281(self):
         #O_WRONLY|O_TRUNC|O_LARGEFILE=33281
         mode = access_mode(33281)
-        self.assertEqual(mode, access_modes.O_WRONLY)
+        self.assertEqual(mode, ACCESS_MODES.O_WRONLY)
 
-# Linux reserves the special, nonstandard access mode 3 (binary 11) in flags to
-# mean: check for read and write permission on the file and return a descriptor
-# that can't be used for reading or writing. This nonstandard access mode is
-# used by some Linux drivers to return a descriptor that is only to be used for
-# device-specific ioctl(2) operations.
+    def test_creation_flags_CREAT_and_TRUNC(self):
+        #O_RDONLY|O_CREAT|O_TRUNC=576=0x240
+        c_flags = creation_flags(576)
+        self.assertSequenceEqual(c_flags, [CREATION_FLAGS.O_TRUNC,
+                                              CREATION_FLAGS.O_CREAT])
+        self.assertSequenceEqual(c_flags, [CREATION_FLAGS.O_CREAT, 
+                                              CREATION_FLAGS.O_TRUNC])
+        
+
+    def test_creation_flags_33281(self):
+        c_flags = creation_flags(33281)
+        self.assertSequenceEqual(c_flags, [CREATION_FLAGS.O_TRUNC])
+
+    # Linux reserves the special, nonstandard access mode 3 (binary 11) in flags to
+    # mean: check for read and write permission on the file and return a descriptor
+    # that can't be used for reading or writing. This nonstandard access mode is
+    # used by some Linux drivers to return a descriptor that is only to be used for
+    # device-specific ioctl(2) operations.
     def test_acceptance_mode_33283_is_None(self):
         mode = access_mode(33283)
         self.assertEqual(mode, None)

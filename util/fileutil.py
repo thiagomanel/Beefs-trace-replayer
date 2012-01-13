30131ae0d8014c9f36ef4d77126aca7c9e0a245e
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2012-01-13 15:30:01 -0300

    access mode ok

--- util/fileutil.py
+++ util/fileutil.py
@@ -1,6 +1,9 @@
 from enum import Enum
 
-access_modes = Enum("O_RDONLY", "O_NONBLOCK", "O_LARGEFILE")
+access_modes = Enum("O_RDONLY", "O_WRONLY", "O_RDWR")
+
+# something outside the range [0,3] is a programming error and the code should fail miserable
+__value2access_modes__ = {0:access_modes.O_RDONLY, 1:access_modes.O_WRONLY, 2:access_modes.O_RDWR, 3:None}
 
 #O_ACCMODE	   0003
 #O_RDONLY	     00
@@ -22,5 +25,9 @@ access_modes = Enum("O_RDONLY", "O_NONBLOCK", "O_LARGEFILE")
    conversion from linux open_mode to its string representation.
    see http://linux.die.net/man/2/open
 """
+def access_mode_bits(flags):
+    return flags & 3
+
 def access_mode(flags):
-    return [access_modes.O_RDONLY, access_modes.O_NONBLOCK, access_modes.O_LARGEFILE]
+#what if a value that does not match ?
+    return __value2access_modes__[access_mode_bits(flags)]
--- util/fileutil_unittest.py
+++ util/fileutil_unittest.py
@@ -11,11 +11,21 @@ class TestFileUtil(unittest.TestCase):
         mode = access_mode(34816)
 	
 	#order is not important
-        self.assertSequenceEqual(mode, [access_modes.O_RDONLY])
+        self.assertEqual(mode, access_modes.O_RDONLY)
 
-#    def test_acceptance_mode_438(self):
-#        modes = from_penmode(438)
-#        self.assertSequenceEqual(modes, [open_modes.O_RDONLY])
+    def test_acceptance_mode_33281(self):
+        #O_WRONLY|O_TRUNC|O_LARGEFILE=33281
+        mode = access_mode(33281)
+        self.assertEqual(mode, access_modes.O_WRONLY)
+
+# Linux reserves the special, nonstandard access mode 3 (binary 11) in flags to
+# mean: check for read and write permission on the file and return a descriptor
+# that can't be used for reading or writing. This nonstandard access mode is
+# used by some Linux drivers to return a descriptor that is only to be used for
+# device-specific ioctl(2) operations.
+    def test_acceptance_mode_33283_is_None(self):
+        mode = access_mode(33283)
+        self.assertEqual(mode, None)
 
     def assertSequenceEqual(self, it1, it2):
         #python 2.7 has new def assertSequenceEqual(self, it1, it2) but i'm with 2.6

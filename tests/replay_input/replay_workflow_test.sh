93c2bf2a1f0b9b98c2c7d091cecc72cefdbd9868
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2012-01-19 10:43:34 -0300

    enabling testin tool setup and teardown

--- tests/replay_input/replay_workflow_test.sh
+++ tests/replay_input/replay_workflow_test.sh
@@ -4,6 +4,57 @@ replayer_path=$1
 workload_input=$2
 verbose=$3
 
-strace -f -e trace=open,close,read,write,mkdir,stat64  $replayer_path $workload_input 2> strace.output
-sed -i 's/stat64/stat/g' strace.output
-python match_syscalls.py $verbose strace.output $workload_input
+usage_msg="Usage: $0 replayer_path workload_input -v [optional]"
+
+pre_replay()
+{
+    if [ -f $workload_input.pre ]
+    then 
+        echo "Running setup $workload_input.pre"
+        $workload_input.pre
+    fi
+}
+
+replay()
+{
+    strace -f -e trace=open,close,read,write,mkdir,stat64  $replayer_path $workload_input 2> strace.output
+    sed -i 's/stat64/stat/g' strace.output
+    python match_syscalls.py $verbose strace.output $workload_input
+}
+
+post_replay()
+{
+    if [ -f $workload_input.post ]
+    then 
+        echo "Running tear down $workload_input.post"
+        $workload_input.post
+    fi
+}
+
+if [ ! -f $replayer_path ]
+then 
+    echo "Missing replayer_path parameter"
+    echo $usage_msg
+    exit 1
+fi
+
+if [ ! -f $workload_input ]
+then 
+    echo "Missing workload_input parameter"
+    echo $usage_msg
+    exit 1
+fi
+
+if [ -f $workload_input.pre ]
+then 
+    echo "Running setup $workload_input.pre"
+    $workload_input.pre
+fi
+
+#setup env. e.g create files and directories
+pre_replay
+
+replay
+
+#tear down env e.g delete files and directories
+post_replay

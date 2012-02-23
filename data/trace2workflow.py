dd5e3e108607f0a7992a5220bd9dd7bf6fc10a27
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2012-02-23 14:56:30 -0300

    converting from trace to workflow works but I missed excluding non /home file

--- data/trace2workflow.py
+++ data/trace2workflow.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     #python order_trace.py < file.clean > file.pidfid_order
     #FIXME do not print pidfid order result, pass them to fsdependency instead
     lines = sys.stdin.readlines()
-    ordered_lines = sorted(order_by_pidfid(lines), key=lambda line: line[0])#sort by _id
-    for o_line in ordered_lines:
+    pidfid_lines = sorted(order_by_pidfid(lines), key=lambda line: line[0])#sort by _id
+    fs_dep_lines = sorted(fs_dependency_order(pidfid_lines), key=lambda line: line[0])#sort by _id
+    for o_line in fs_dep_lines:
         sys.stdout.write(" ".join(map(str, o_line)))

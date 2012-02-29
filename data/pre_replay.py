2dd521a2a21530134142a94e26979e29ce9ff91a
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2012-02-29 18:07:30 -0300

    going home

--- data/pre_replay.py
+++ data/pre_replay.py
@@ -5,6 +5,7 @@ from workflow_objects import objects
 def fs_objects_to_create(workflow_lines):
 
     def trace_walk(workflow_lines, timestamp):
+        """ walk through directories created before timestamp """
 
         def syscall_timestamp(syscall):
             # it should be moved to an util module (testing tool has something related to it) FIXME
@@ -12,15 +13,19 @@ def fs_objects_to_create(workflow_lines):
             timestamp_tokens = syscall.split()[5].split("-")
             return (int(timestamp_tokens[0]), int(timestamp_tokens[1]))
 
-        def created_before(timestamp, workflow_line):
-            (_id, n_parents, parents, n_children, children, syscall) = objects(workflow_line)
+        def created_before(timestamp, syscall):
             return syscall_timestamp(syscall)[0] < timestamp[0]
 
+        to_create = []
+        
         w_tree = graph(workflow_lines)
         for node, children in bfs(w_tree, 1):
-            if created_before(timestamp, node):
+            node_objs = objects(node)
+            node_syscall = node_objs[-1]
+            children_objs = map(objects, children)
+            if created_before(timestamp, node_syscall):
                 old_children = [child for child in children 
                                 if created_before(timestamp, child)]
-                yield node, old_chilren#FIXME to add fd object type (f or d), do not know where
+                node, old_chilren#FIXME to add fd object type (f or d), do not know where
 
     return []

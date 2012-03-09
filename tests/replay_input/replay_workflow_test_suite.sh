0df38d667ed7054eec0e0b0672bbf39234779635
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2012-03-09 20:13:11 -0300

    testing timing

--- tests/replay_input/replay_workflow_test_suite.sh
+++ tests/replay_input/replay_workflow_test_suite.sh
@@ -4,6 +4,7 @@ bash ./replay_workflow_test.sh ../../beefs_replayer workflow_samples/workflow_si
 bash ./replay_workflow_test.sh ../../beefs_replayer workflow_samples/workflow_single_command_mkdir
 bash ./replay_workflow_test.sh ../../beefs_replayer workflow_samples/workflow_single_command_stat
 bash ./replay_workflow_test.sh ../../beefs_replayer workflow_samples/workflow_2_sequencial_command_mkdir
+bash ./replay_workflow_test.sh ../../beefs_replayer workflow_samples/workflow_2_sequencial_command_mkdir_with_large_wait
 bash ./replay_workflow_test.sh ../../beefs_replayer workflow_samples/workflow_48_sequencial_command_mkdir
 bash ./replay_workflow_test.sh ../../beefs_replayer workflow_samples/workflow_sequencial_open_read_close_same_file
 bash ./replay_workflow_test.sh ../../beefs_replayer workflow_samples/workflow_sequencial_open_write_close_same_file

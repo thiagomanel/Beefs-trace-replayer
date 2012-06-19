0ba83075608a16d3b9280a8b3fb581872b79e6c3
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2012-06-19 18:07:27 -0300

    going home

--- experiments/beefs_bootstrapper.py
+++ experiments/beefs_bootstrapper.py
@@ -8,12 +8,17 @@ class Entry():
            self.sto_id = sto_id
 
     def __init__(self, fullpath, ftype, replicas):
+        if not ftype in ("f", "d"):
+            raise ValueError("We allow f or d arg: %s", ftype)
         self.fullpath = fullpath
         self.ftype = ftype
         self.replicas = replicas
 
+    def __str__(self):
+        return "fullpath=<%s> ftype=<%s>" % (self.fullpath, self.ftype)
+
     def is_dir(self):
-        return self.ftype is "f"
+        return self.ftype is "d"
 
 def distribution(namespace_path, rlevel):
     """ It creates a beefs data distribution, sto location and replication info,
@@ -35,7 +40,43 @@ def distribution(namespace_path, rlevel):
                   each file under namespace_path we an Entry object. If a file
                   is a dir, replicas is a empty collection on Entry object
     """
+
+    def dentry(fullpath):
+        return Entry(fullpath, "d", [])
+    def fentry(fullpath, rlevel):
+        if rlevel < 0:
+            raise ValueError("rlevel should not be negative %d", rlevel)
+
+        replicas = []
+        for i in range(rlevel):
+            replicas.append(Entry.Replica(None, None, None))
+
+        return Entry(fullpath, "f", replicas)
+
     graph = {}
+    entry_by_path = {}
     for root, dirs, files in os.walk(namespace_path):
-        graph[root] = [_dir for _dir in dirs if _dir]
+        if not root in entry_by_path:
+            entry_by_path[root] = dentry(root)
+
+        root_entry = entry_by_path[root]
+        graph[root_entry] = []
+
+        #FIXME remove this duplication
+        for _dir in dirs:
+            if _dir:
+                fullpath = os.path.join(root, _dir)
+                if not _dir in entry_by_path:
+                    entry = dentry(fullpath)
+                    entry_by_path[fullpath] = entry
+                graph[root_entry].append(entry_by_path[fullpath])
+
+        for _file in files:
+            if _file:
+                fullpath = os.path.join(root, _file)
+                if not _file in entry_by_path:
+                    entry = fentry(fullpath, rlevel)
+                    entry_by_path[fullpath] = entry
+                graph[root_entry].append(entry_by_path[fullpath])
+		
     return graph
--- experiments/beefs_bootstrapper_test.py
+++ experiments/beefs_bootstrapper_test.py
@@ -20,6 +20,12 @@ class TestDistribution(unittest.TestCase):
         for func in reversed(self._on_teardown):
             func()
 
+    def entries_by_path(self, entries):
+        by_path = {}
+        for entry in entries:
+            by_path[entry.fullpath] = entry
+        return by_path
+
     def test_distribution_directories_and_files(self):
 
         #    base_dir
@@ -45,9 +51,74 @@ class TestDistribution(unittest.TestCase):
         afile = os.path.join(non_empty_dir, "file")
         open(afile, 'w').close()
 
-        dist_tree = distribution(temp_dir, 2)
+        replication_level = 2
+        entry_tree = distribution(temp_dir, replication_level)
+        self.assertEquals(5, len(entry_tree.keys()))
+
+        entries_by_path = self.entries_by_path(entry_tree.keys())
         for _dir in dirs:
-            self.assertTrue(_dir in dist_tree)
+            self.assertTrue(_dir in entries_by_path)
+            entry = entries_by_path[_dir]
+            self.assertTrue(entry.is_dir())
+            self.assertFalse(entry.replicas)#empty evaluates to false
+
+        #base_dir/manel subtree
+        manel_entry = entries_by_path[manel_dir]
+        manel_entry_children = self.entries_by_path(entry_tree[manel_entry])
+        self.assertEquals(2, len(manel_entry_children.keys()))
+        self.assertTrue(empty_dir in manel_entry_children)
+        self.assertTrue(non_empty_dir in manel_entry_children)
+
+        empty_dir_entry = manel_entry_children[empty_dir]
+        self.assertTrue(empty_dir_entry.is_dir())
+        empty_dir_entry_children = self.entries_by_path(
+                                       entry_tree[empty_dir_entry])
+        self.assertEquals(0, len(empty_dir_entry_children))
+
+        non_empty_dir_entry = manel_entry_children[non_empty_dir]
+        non_empty_dir_entry_children = self.entries_by_path(
+                                           entry_tree[non_empty_dir_entry])
+        self.assertEquals(1, len(non_empty_dir_entry_children))
+        self.assertTrue(afile in non_empty_dir_entry_children)
+        afile_entry = non_empty_dir_entry_children[afile]
+        self.assertFalse(afile_entry.is_dir())
+        self.assertEquals(replication_level, len(afile_entry.replicas))
+
+        # base_dir/empty_user subtree
+        empty_user_entry = entries_by_path[empty_user_dir]
+        self.assertTrue(empty_user_entry.is_dir())
+        empty_user_entry_children = self.entries_by_path(
+                                             entry_tree[empty_user_entry])
+        self.assertFalse(empty_user_entry_children)#empty evaluates to false
+
+    def test_replica_distribution(self):
+        temp_dir = self.make_temp_dir()
+        num_dirs = 10
+        dirs = [os.path.join(temp_dir, str(_dir)) for _dir in range(num_dirs)]
+
+        files_per_dir = 10
+        files = []
+        for _dir in dirs:
+            files.extend([os.path.join(_dir, str(_file)) 
+                             for _file in range(files_per_dir)])
+        for _dir in dirs:
+            os.mkdir(_dir)
+        for _file in files:
+            open(_file, 'w').close()
+
+        replication_level = 2
+        dist_tree = distribution(temp_dir, replication_level)
+
+        #all primary replicas should be stored in the same osd
+        entries_by_path = self.entries_by_path(entry_tree.keys())
+        for _dir in dirs:
+            dir_entry = entries_by_path[_dir]
+            dir_children_entries = dist_tree[dir_entry]
+            self.assertEquals(file_per_dir, len(dir_children_entries))
+            dir_children_primreplicas = [entry.replicas[0].osd_id
+                                            for entry in dir_children_entries]
+            #all primary replicas should be stored in the same osd
+            self.assertEquals(1, set(osd_id))
 
 
 if __name__ == '__main__':

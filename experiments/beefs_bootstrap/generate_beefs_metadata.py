3dc33ea3952bb9f8b1b8f70dfb0e19d466fda44b
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2012-06-28 17:56:07 -0300

    queenbee bootstrap works

--- experiments/beefs_bootstrap/generate_beefs_metadata.py
+++ experiments/beefs_bootstrap/generate_beefs_metadata.py
@@ -21,10 +21,31 @@ def replace_osdid(line, id_to_replacement):
          new_string = new_string.replace(old_str, replacement)        
      return new_string
 
-def main(boot_data_path, output_dir, network_id_path):
+def remove_local_path(local_path_head, line):
+    """ It removes the local path head. For example, with 
+        local_path_head="/local/nfs_manel" and 
+        line="{"path": "/local/nfs_manel/patrickjem/coreutils-5.0/m4", 
+        "type": "d", 
+        "inodeId": "29c26445-fbe4-414c-8184-031763d9a410", 
+        "group": {}, "parentId": "b890b2d0-13ba-4ebf-8e08-619b3813752c"
+       }"
+
+       we get
+
+       {"path": "/patrickjem/coreutils-5.0/m4", 
+        "type": "d", 
+        "inodeId": "29c26445-fbe4-414c-8184-031763d9a410", 
+        "group": {}, "parentId": "b890b2d0-13ba-4ebf-8e08-619b3813752c"
+       }
+    """
+    return line.replace(local_path_head, "")
+
+def main(server_export_dir, boot_data_path, output_dir, network_id_path):
     """ It creates beefs metadata based on generate_boot_data.py output
         
         Args:
+            server_export_dir (str): Namespace is stored under this dir at 
+                                     server side.
             boot_data_path (str): path to generate_boot_data.py output.
             output_dir (str): path to a directory to store generated beefs
                               metadata.
@@ -34,8 +55,10 @@ def main(boot_data_path, output_dir, network_id_path):
     """
     def create_replacement(network_id_path):
         def parse(line):
-            #from /local/nfs_manel/igorvcs	3ad4fafb-0be0-49b3-ad0a-cda1c85fb6fd {"hostname": "host1", "port": 1111, "dataPort": 2222
-            #it returns ("3ad4fafb-0be0-49b3-ad0a-cda1c85fb6fd", "{"hostname": "host1",     "port": 1111, "dataPort": 2222}")a
+            #when
+            #/path 3ad4fafb-0be0-49b3-ad0a-cda1c85fb6fd {"hostname": "host1", "port": 1, "dataPort": 2}
+            #then
+            #("3ad4fafb-0be0-49b3-ad0a-cda1c85fb6fd", "{"hostname": "host1", "port": 1, "dataPort": 2}")
             tokens = line.split()
             uuid = tokens[1]
             network_id = " ".join(tokens[2:])
@@ -46,33 +69,65 @@ def main(boot_data_path, output_dir, network_id_path):
             for line in network_id_data:
                 replacement.update(parse(line))
         return replacement
+
+    def format_queenbee_input(replacement, exp_dir, unform_path, form_path):
+
+        """ It formats boot data. New format has network ids instead uuid,
+            paths had their leading path removed of "exp_dir". Root's inode_id
+            is replace by "0" to follow beefs internals.
+            Args:
+                replacement (dict): a {uuid:network_id} dict
+                exp_dir (src): leading path to be remove on formated file
+                unform_path (str): path to unformatted input file
+                form_path (str): formatted data will be store here
+        """
+        def inode_id(line):
+            return line.split("inodeId\": ")[1].split("\"")[1]
+
+        def path(line):
+            return line.split("path\": ")[1].split("\"")[1]
+
+        def replace_inode_id(line, old_id, new_id):
+            return line.replace(old_id, new_id)
+
+        with open(unform_path) as base_boot_data:
+            root_inode_id = None
+            formatted_lines = []
+
+            for old_line in base_boot_data:
+                new_line = replace_osdid(old_line, replacement)
+                new_line = remove_local_path(server_export_dir, new_line)
+                formatted_lines.append(new_line)
+                if path(new_line) == "/":
+                    root_inode_id = inode_id(new_line)
+
+            with open(form_path, 'w') as form_data:
+                for line in formatted_lines:
+                    final_line = replace_inode_id(line, root_inode_id, "0")
+                    form_data.write(final_line)
                 
     iso = "ISO-8859-1"
-#    with open(boot_data_path) as boot_data:
-#        entries = [Entry.from_json(json.loads(entry, encoding=iso)) 
-#                       for entry in boot_data]
-#
-#        generate_data_servers_metadata(entries, output_dir)
-#
+    with open(boot_data_path) as boot_data:
+        entries = [Entry.from_json(json.loads(entry, encoding=iso)) 
+                       for entry in boot_data]
+
+        generate_data_servers_metadata(entries, output_dir)
+
     queenbee_out_dir = os.path.join(output_dir, "queenbee_metadata")
     if not os.path.exists(queenbee_out_dir):
         os.mkdir(queenbee_out_dir)
-    queenbee_boot_data_path = os.path.join(queenbee_out_dir, "queenbee.boot")
+    formatted_boot_path = os.path.join(queenbee_out_dir, "queenbee.boot")
 
     replacement = create_replacement(network_id_path)
-
-    with open(queenbee_boot_data_path, 'w') as queenbee_boot_data:
-        with open(boot_data_path) as base_boot_data:
-            for old_line in base_boot_data:
-                new_line = replace_osdid(old_line, replacement)
-                queenbee_boot_data.write(new_line)
-
-    generate_queenbee_metadata(queenbee_boot_data_path, queenbee_out_dir)
+    format_queenbee_input(replacement, server_export_dir, boot_data_path,
+                          formatted_boot_path)
+    generate_queenbee_metadata(formatted_boot_path, queenbee_out_dir)
 
 if __name__ == "__main__":
 
-    boot_data_path = sys.argv[1]
-    output_dir = sys.argv[2]
-    network_id_path = sys.argv[3]
+    export_dir = sys.argv[1]
+    boot_data_path = sys.argv[2]
+    output_dir = sys.argv[3]
+    network_id_path = sys.argv[4]
 
-    main(boot_data_path, output_dir, network_id_path)
+    main(export_dir, boot_data_path, output_dir, network_id_path)

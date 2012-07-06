d511a41ba55ace3da98e37b9824d68d21e214777
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2012-07-06 00:16:07 -0300

    coordinator seems ok, we need to preserver some stubs

--- experiments/beefs_exp/coordinator.py
+++ experiments/beefs_exp/coordinator.py
@@ -5,7 +5,6 @@ import random
 import subprocess
 from time import sleep
 
-#we have stuff running in cloudlgy
 EXPERIMENT_INPUT_DIR="/tmp/home/thiagoepdc/experiments/beefs"
 EXP_OUT_DIR = EXPERIMENT_INPUT_DIR + "/results/"
 
@@ -15,7 +14,7 @@ LAN_INPUT_DIR="/home/thiagoepdc/experiments/"
 class Component:
     DATA_SERVER, CLIENT, META_SERVER = range(3)
 
-class Deploy()
+class Deploy():
     def __init__(self, ms_node, node2replay_input, install_dir, mount_point):
         """
            Args:
@@ -46,8 +45,8 @@ class Deploy()
     def umount(self, node):
         #I do not like much this check within the call (and raising an exception
         #but main code is a way more cleaner because of this.
-        def do_umount(self, node):
-            return execute(" ".join(["umount", self.mount_point]), node)
+        def do_umount(node, mount_point):
+            return execute(" ".join(["umount", mount_point]), node)
 
         def check(node):
             #out, err, rcode = execute("mount | grep 150.165.85.239:/local/nfs_manel", )
@@ -56,35 +55,40 @@ class Deploy()
 
         sys.stdout.write("umount node=%s mount_point=%s\n"
                           % (node, self.mount_point))
-        out, err, rvalue = self.do_umount(node)
+        out, err, rvalue = do_umount(node, self.mount_point)
         if not check(node):
             raise Exception("Unable to umount node=%s out=%s err=%s"
                             % (node, out, err))
         return out, err, rvalue
 
-    def start(self, node):
+    def start(self, component_type, node):
         if component_type == Component.DATA_SERVER:
             remote_command = " ".join([self.beefs_script, "start", "honeycomb"])
+            execute(remote_command, node, delay=None)
         elif component_type == Component.META_SERVER:
             remote_command = " ".join([self.beefs_script, "start", "queenbee"])
-        execute(remote_command, node, delay=None):
-
+            execute(remote_command, node, delay=None)
+	
     def stop(self, component_type, node):
 
-        def do_stop(self, component_type, node):
+        def do_stop(component_type, node):
             if component_type == Component.DATA_SERVER:
                 remote_command = " ".join([self.beefs_script, "stop", "honeycomb"])
+                return execute(remote_command, node, delay=None)
             elif component_type == Component.META_SERVER:
                 remote_command = " ".join([self.beefs_script, "stop", "queenbee"])
-            return execute(remote_command, node, delay=None):
+                return execute(remote_command, node, delay=None)
+            elif component_type == Component.CLIENT:
+                remote_command = " ".join([self.beefs_script, "stop", "honeybee"])
+                return execute(remote_command, node, delay=None)
 
-        def check(self, node):
-            pass 
+        def check(node):#FIXME
+            return True
 
-        sys.stdout.write("stop component=%n node=%s\n" % (component_type, node))
-        out, err, rvalue = self.do_stop(component_type, node)
+        sys.stdout.write("stop component=%d node=%s\n" % (component_type, node))
+        out, err, rvalue = do_stop(component_type, node)
         if not check(node):
-            raise Exception("Unable to stop component=%n node=%s out=%s err=%s"
+            raise Exception("Unable to stop component=%d node=%s out=%s err=%s"
                             % (component_type, node, out, err))
         return out, err, rvalue
 
@@ -104,11 +108,11 @@ class Deploy()
             rollback_metadata(node)
 
     def wait_and_start_replay(self, node, deadline, out_path, err_path):
+        sys.stdout.write("wait and start node=%s\n" % (node))
         input_path = self.node2replay_input[node]
-
         cmd = " ".join(["bash", 
                         self.wait_and_replay_script_path,
-                        str(deadline), self.path_beefs_replayer_path, input_path,
+                        str(deadline), self.beefs_replayer_path, input_path,
                         out_path, err_path, "&"])
 
         return execute(cmd, node)
@@ -122,14 +126,19 @@ class Deploy()
             out, err, rcod = execute("ps xau | grep beefs_replayer | grep -v grep", addr)
             return out#if it is running, out it not emptu, so it's is true
 
-        #wait for replay termination in all nodes
-        while any([replayer_is_running(addr) for node in self.ds_nodes()]):
+        sys.stdout.write("wait for replay termination in all nodes\n")
+        while any([replayer_is_running(node) for node in self.ds_nodes()]):
             sys.stdout.write("Waiting worker nodes job termination\n")
             sleep(30)
 
+    def copy_result(self, node):
+        sys.stdout.write("collection results " + node + "\n")
+        out, err, rcode = execute("mv /tmp/*replay* " + EXP_OUT_DIR, node)
+        print out, err, rcode
+
 def execute(remote_command, machine_addr, delay=None):
     process = subprocess.Popen(" ".join(["ssh",
-	                                     machine_addr,
+	                                     "root@" + machine_addr,
                                              remote_command]),
 					shell=True,
                                         stdout=subprocess.PIPE,
@@ -137,16 +146,10 @@ def execute(remote_command, machine_addr, delay=None):
     out, err = process.communicate()
     return out, err, process.returncode
 
-def copy_results(node):
-    for addr, r_input in machines_addr2replay_input.iteritems():
-        sys.stdout.write("collection results " + addr + "\n")
-        out, err, rcode = execute("mv /tmp/*replay* " + output_dir, addr)
-        print out, err, rcode
-
 def mount_client(addr, mount_point):
     return execute(" ".join(["mount", mount_point]), addr)
 
-def main(num_samples, ms_node, node2replay_input, mount_point):
+def main(num_samples, ms_node, node2replay_input, mount_point, install_dir):
 
     def base_out_path(node, sample):
         #/tmp/node.sample.random
@@ -157,36 +160,36 @@ def main(num_samples, ms_node, node2replay_input, mount_point):
 
     deploy = Deploy(ms_node, node2replay_input, install_dir, mount_point)
 
-    nodes = cluster_node2trace_input.keys()#sugar
+    nodes = node2replay_input.keys()#sugar
     for sample in range(num_samples):
         sys.stdout.write("Running sample " +  str(sample) + "\n")
         for node in nodes:
-            umount(node)
-            stop(Component.CLIENT, node)
-            stop(Component.DATA_SERVER, node)
-            rollback(Component.DATA_SERVER, node)
+            deploy.umount(node)
+            deploy.stop(Component.CLIENT, node)
+            deploy.stop(Component.DATA_SERVER, node)
+            deploy.rollback(Component.DATA_SERVER, node)
 
-         stop(Component.META_SERVER, queenbee_node)
-         rollback(Component.META_SERVER, queenbee_node)
+        deploy.stop(Component.META_SERVER, queenbee_node)
+        deploy.rollback(Component.META_SERVER, queenbee_node)
  
-         start(Component.META_SERVER, node)
-         for node in nodes:
-            start(Component.DATA_SERVER, node)
-            start(Component.CLIENT, node)
+        deploy.start(Component.META_SERVER, queenbee_node)
+        for node in nodes:
+            deploy.start(Component.DATA_SERVER, node)
+            deploy.start(Component.CLIENT, node)
 
-         now_epoch_secs = time(nodes[0])#nodes are sync'ed, just pick one
-         deadline = now_epoch_secs + 30
-         sys.stdout.write("now is: " + str(now_epoch_secs) +
+        now_epoch_secs = deploy.time(nodes[0])#nodes are sync'ed, just pick one
+        deadline = now_epoch_secs + 30
+        sys.stdout.write("now is: " + str(now_epoch_secs) +
                           " deadline will be " + str(deadline) + "\n")
-         for node in nodes:#it'se better to have a separated for, help sync
-             base_out = base_out_path(node, sample)
-             out_file = base_out + ".replay.out"
-             err_file = base_out + ".replay.err"
-             wait_and_start_replay(node, deadline, out_file, err_file)
+        for node in nodes:#it'se better to have a separated for, help sync
+            base_out = base_out_path(node, sample)
+            out_file = base_out + ".replay.out"
+            err_file = base_out + ".replay.err"
+            deploy.wait_and_start_replay(node, deadline, out_file, err_file)
 
-         wait_replay_end()
-         for node in nodes:
-            copy_results(node)
+        deploy.wait_replay_end()
+        for node in nodes:
+            deploy.copy_result(node)
 
 if __name__ == "__main__":
     """
@@ -198,13 +201,19 @@ if __name__ == "__main__":
     #logging.basicConfig(filename='example.log',level=logging.DEBUG)
     #logging.info('So should this')
 
-    if len(sys.argv) < 5:
-        sys.stderr.write("Usage: python coordinator.py num_samples machines_file trace_file mount_point\n")
+    if len(sys.argv) < 7:
+        sys.stderr.write("Usage: python coordinator.py num_samples queenbee_hostname machines_file trace_file mount_point beefs_dir_on_vm\n")
         sys.exit(-1)
 
     num_samples = int(sys.argv[1])
-    with open(sys.argv[2]) as machines:
-        with open(sys.argv[3]) as traces:
+    queenbee_node = sys.argv[2]
+    machine_file = sys.argv[3]
+    trace_file = sys.argv[4]
+    mount_point = sys.argv[5]
+    beefs_dir_vm = sys.argv[6]
+
+    with open(machine_file) as machines:
+        with open(trace_file) as traces:
             machines_addr2replay_input = {}
             for addr in machines:
                 trace_line = traces.readline()
@@ -212,6 +221,4 @@ if __name__ == "__main__":
             machines_addr = machines_addr2replay_input.keys()
 
     sys.stdout.write(" ".join(["loaded machines", str(machines_addr), "\n"]))
-
-    mount_point = sys.argv[4]
-    main(num_samples, machines_addr2replay_input, mount_point)
+    main(num_samples, queenbee_node, machines_addr2replay_input, mount_point, beefs_dir_vm)

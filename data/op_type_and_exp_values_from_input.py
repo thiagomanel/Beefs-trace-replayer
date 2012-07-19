3068e253acc0557d3d71c6278a6ba3a88ce5a05f
Thiago Emmanuel / Projeto Smart Pumping <thiagoepdc@tarpon.(none)>
2012-07-19 16:31:24 -0300

    plotting and result checks

---
+++ data/op_type_and_exp_values_from_input.py
@@ -0,0 +1,17 @@
+import sys
+import json
+from workflow import *
+
+if __name__ == "__main__":
+    """ It outputs the operation and expected return value for
+        replay input operations.
+
+        Usage: python input_expected_value.py < input_file > output
+        Output format: operation_type\texpected_rvalue
+    """
+    sys.stdin.readline()#excluding header
+    w_lines = [WorkflowLine.from_json(json.loads(line)) for line in sys.stdin] 
+    for w_line in w_lines:
+        call_type = w_line.clean_call.call
+        rvalue = w_line.clean_call.rvalue
+        sys.stdout.write("\t".join([call_type, rvalue]) + "\n")

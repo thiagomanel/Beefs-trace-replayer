bd4e52e9ca5605a7ed1a38102bf72ff841a0e54d
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2012-08-15 17:30:20 -0300

    it works, going to analyse data now

--- data/timing.py
+++ data/timing.py
@@ -3,33 +3,21 @@ import sys
 from bfs import *
 from workflow import *
 
-if __name__ == "__main__":
-    """
-       It changes the expected begin of trace calls to reflect 
-       timing polices.
-
-       Usage: python timing.py workflow_path > new_workflow
-    """
-    def parse(line):
-        entry = json.loads(line)
-        return WorkflowLine.from_json(entry)
-
+def update(entries):
     def stamp(workflow_entry):
         stamp = workflow_entry.clean_call.stamp.split("-")
-        return float(stamp[0]), long(stamp[1])
+        return long(stamp[0]), long(stamp[1])
 
     def update_stamp(workflow_entry, workflow):
-
         def entry_by_id(entry_id):
             return workflow[entry_id]
 
         def check_new_begin(new_begin, entry):
             old_entry_begin, old_entry_elapsed = stamp(entry)
             if old_entry_begin <= new_entry_begin:
-                sys.stderr.write("new begin=%d is latter than old=%d id=%s\n" \
+                raise Exception("new begin=%d is latter than old=%d id=%s\n" \
                                   % (new_entry_begin, old_entry_begin,
                                      workflow_entry._id))
-                exit(1)
 
         def parent(entry):
             def finish_later(entries_id):
@@ -52,11 +40,26 @@ if __name__ == "__main__":
             old_begin, old_elapsed = stamp(entry)
             workflow_entry.clean_call.stamp = "-".join([str(new_entry_begin), \
                                                         str(old_elapsed)])
-        
+    
+    for entry in entries.itervalues():
+        update_stamp(entry, entries)
+
+if __name__ == "__main__":
+    """
+       It changes the expected begin of trace calls to reflect 
+       timing polices.
+
+       Usage: python timing.py workflow_path > new_workflow
+    """
+    def parse(line):
+        entry = json.loads(line)
+        return WorkflowLine.from_json(entry)
+
     with open(sys.argv[1]) as workflow_file:
         workflow = {}
         for line in workflow_file:
             entry = parse(line)
             workflow[entry._id] = entry
-            update_stamp(entry, workflow)
+        update(workflow)
+        for entry in workflow.itervalues():    
             print json.dumps(entry.json())

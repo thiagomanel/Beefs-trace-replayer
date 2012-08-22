92602368c4855cedf36899e4c3c98ba11c5ae240
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2012-08-22 10:39:24 -0300

    plot done

--- data/timing_policy/plot.r
+++ data/timing_policy/plot.r
@@ -1,17 +1,14 @@
 library("ggplot2")
 
-/*1319217042997604	2	conservative	abelhinha */
 Args <- commandArgs(TRUE)
 file=Args[1]
 
-data <-read.table(file)
-/*png(paste(file, ".png", sep=""))
-
-min_stamp <- min(data$V1)
-
-p <- ggplot(data, aes((data$V1 - min_stamp), data$V2))
-p + geom_point(aes(colour = data$V3))*/
+event.load <-read.table(file, header=T)
+# png(paste(file, ".png", sep=""))
+# min_stamp <- min(data$V1)
+# p <- ggplot(data, aes((data$V1 - min_stamp), data$V2))
+# p + geom_point(aes(colour = data$V3))
 
 png(paste(file, ".png", sep=""))
-b <- ggplot(data, aes(factor(data$V3), data$V2)) + facet_grid(. ~ data$V4)
-b + geom_boxplot()
+b <- ggplot(event.load, aes(factor(policy), count)) + geom_boxplot()
+b + facet_wrap(~ machine, scales = "free")

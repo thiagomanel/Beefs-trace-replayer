b7d7094e3db7e1a762a1b2acbbdcb2269b3e77ad
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2013-04-03 22:10:33 -0300

    refactoring old workflow converver to new ordering and timing polices

--- data/trace2workflow.py
+++ data/trace2workflow.py
@@ -4,28 +4,27 @@ from clean_trace import *
 
 if __name__ == "__main__":
     """
-       It converts from a cleaned data file to workflow data. To do so, input is ordered
-       using pid fid order and fs dependency algorithms.
+       It converts from a cleaned data file to workflow data.
 
-       Usage: python trace2workflow.py < file.clean > file.pidfid_order
+       Usage: python trace2workflow.py "wfs"|"c" < file.clean > output
+            wfs and c are Zhu's fs dependency (we call it weak fs in code) and
+            conservative polices
     """
-    #FIXME do not print pidfid order result, pass them to fsdependency instead
     lines = sys.stdin.readlines()
     cleaned_calls = [CleanCall.from_str(line) for line in lines]
+    workflow_lines = []
+    for (_id, cleaned_call) in enumerate(cleaned_calls):
+        workflow_lines.append(WorkflowLine(_id + 1, [], [], cleaned_call))
 
-    pidfid_lines = sorted(order_by_pidfid(cleaned_calls),
-                             key=lambda line: line._id)#sort by _id
-
-    fs_dep_lines = sorted(fs_dependency_order(pidfid_lines), 
-                             key=lambda line: line._id)#sort by _id
+    police = sys.argv[1]
+    if police == "wfs":
+         new_lines = weak_fs_dependency_sort(w_lines)
+    elif police == "c":
+        new_lines = conservative_sort(w_lines)
 
     #json dumps cannot handle our data
-    sys.stdout.write("[\n")
-    sys.stdout.write(str(fs_dep_lines[0]))
-    for w_line in fs_dep_lines[1:]:
-        try:
-            sys.stdout.write(",\n" + str(w_line))
-        except ValueError:
-            sys.stderr.write(str(w_line.clean_call))
-            raise 
-    sys.stdout.write("\n]")
+    sys.stdout.write(str(len(new_lines)))
+    for w_line in new_lines:
+         #this json is different from default workflow  __str__ no \n lines
+         json_str = json.dumps(w_line.json())
+         sys.stdout.write("\n" + json_str.strip())

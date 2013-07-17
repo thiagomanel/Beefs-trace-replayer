f1ffcac79db5ba55ed2bfffb5b455050e147ccb1
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2013-07-17 11:00:44 -0300

    a directory to keep replayer verification scripts

---
+++ verification/accuracy/issue_error.py
@@ -0,0 +1,9 @@
+import sys
+
+if __name__ == "__main__":
+    """
+        It computes the issue error: the time interval between
+        the expected and actual dispatch timestamps;
+
+        Usage: python $0 < replay.out
+    """

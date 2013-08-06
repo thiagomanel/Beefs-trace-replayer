eab7f9d3ef84fdbb71102e1f02633aba25a53a13
Thiago Emmanuel Pereira <thiago.manel@gmail.com>
2013-08-06 11:35:15 -0300

    performance improvement of ordering algorithm

--- data/workflow.py
+++ data/workflow.py
@@ -162,8 +162,10 @@ def write_semantics(call):
 
 def join(father, son):
     if son:
-        father.children.append(son._id)
-        son.parents.append(father._id)
+        if not son._id in father.children:
+            father.children.append(son._id)
+        if not father._id in son.parents:
+            son.parents.append(father._id)
 
 def update_dependency(w_line_to_update, w_line_target_parents):
     for parent in reversed(w_line_target_parents):
@@ -174,103 +176,9 @@ def update_dependency(w_line_to_update, w_line_target_parents):
 
 def sfs(workflow_lines):
     """
-        TODO:
+        FS dependency plus tid, pid ordering
     """
-    def sfs_update_dependency(w_line_to_update, w_line_target_parents):
-
-        def same_thread(w1, w2):
-            return pidfidprocess(w1) == pidfidprocess(w2)
-        def orphan(w):
-            return not w.parents
-
-        for parent in reversed(w_line_target_parents):
-            clean_call = parent.clean_call
-            if write_semantics(clean_call.call) and not same_thread(parent, w_line_to_update):
-                join(parent, w_line_to_update)
-                break
-
-    def fs_obj(clean_call, pid_fid_to_path):
-
-        if (clean_call.call == "mkdir") or (clean_call.call == "unlink") \
-           or (clean_call.call == "rmdir"):
-            filepath = clean_call.fullpath()
-            parent = parent_path(filepath)
-            return [filepath, parent]
-        elif (clean_call.call == "stat") or (clean_call.call == "open") \
-           or (clean_call.call == "write") or (clean_call.call == "read") \
-           or (clean_call.call == "llseek"):
-            return [clean_call.fullpath()]
-        elif (clean_call.call == "fstat"):
-            pidfid = (clean_call.pid, clean_call.fd())
-            filepath = pid_fid_to_path[pidfid]
-            return [filepath]
-        else:
-            raise Exception("unsupported operation " + str(clean_call))
-
-    def update_session(wline, session_table):
-        clean_call = wline.clean_call
-        pidfd = (clean_call.pid, clean_call.fd())
-        if not pidfd in session_table:
-            session_table[pidfd] = []
-        session_table[pidfd].append(wline)
-        return pidfd
-
-    sort_by_pidfid(workflow_lines)
-
-    lines_by_fs_obj = {}
-    pid_fd_sessions = {}
-
-    pid_fd_to_path = {}
-
-    for w_line in workflow_lines:
-
-        clean_call = w_line.clean_call
-
-        if clean_call.fd_based():
-            pidfd = update_session(w_line, pid_fd_sessions)
-
-        if clean_call.call == "open":
-            pid_fd_to_path[pidfd] = clean_call.fullpath()
-        elif clean_call.call == "close":
-            del pid_fd_to_path[pidfd]
-
-        if not clean_call.call == "close":#see CLOSE_CASE below
-            for obj in fs_obj(clean_call, pid_fd_to_path):
-                if not obj in lines_by_fs_obj:
-                    lines_by_fs_obj[obj] = Operations()
-                lines_by_fs_obj[obj].append(w_line)
-
-    for (obj, operations) in lines_by_fs_obj.iteritems():
-        for i in reversed(xrange(len(operations))):
-            sfs_update_dependency(operations[i], operations[:i])
-
-    session_counter = -1
-    for (pidfid, operations) in pid_fd_sessions.iteritems():
-        #note it's possible to have more than one open-to-close session for the
-        #same pid_fd e.g:
-        #pid=111 open("/foo") -> 4
-        #pid=111 close(4)
-        #pid=111 open("/baa") -> 4
-        #pid=111 close(4)
-        if not operations[0].clean_call.call == "open":
-            raise Exception("Something got wrong ! We should start a session with a open syscall")
-
-        for w_line in operations:
-            if w_line.clean_call.call == "open":
-                session_counter = session_counter + 1
-            w_line.session_id = session_counter
-
-    #CLOSE CASE: we keep operations sessions (from open to close).
-    #we make close's parent its antecessor on session
-    for (pidfid, operations) in pid_fd_sessions.iteritems():
-        for i in reversed(xrange(len(operations))):
-            current_wline = operations[i]
-            if (current_wline.clean_call.call == "close"):
-               #assuming a close cannot be the 0th on operations list
-               #this code works
-               parent_candidate = operations[i - 1]
-               if not parent_candidate.clean_call.call == "close":
-                   join(parent_candidate, current_wline)
+    generic_fs_dependency_sort(workflow_lines, True)
 
 def weak_fs_dependency_sort(workflow_lines):
     """
@@ -280,6 +188,10 @@ def weak_fs_dependency_sort(workflow_lines):
         It deviates from the fs_dependency_order in such it does not depend on
         pid/fid ordering
     """
+    generic_fs_dependency_sort(workflow_lines, False)
+
+def generic_fs_dependency_sort(workflow_lines, sort_by_pidtid):
+
     def fs_obj(clean_call, pid_fid_to_path):
 
         if (clean_call.call == "mkdir") or (clean_call.call == "unlink") \
@@ -306,6 +218,9 @@ def weak_fs_dependency_sort(workflow_lines):
         session_table[pidfd].append(wline)
         return pidfd
 
+    if sort_by_pidtid:
+        sort_by_pidfid(workflow_lines)
+
     lines_by_fs_obj = {}
     pid_fd_sessions = {}
 
@@ -330,8 +245,13 @@ def weak_fs_dependency_sort(workflow_lines):
                 lines_by_fs_obj[obj].append(w_line)
 
     for (obj, operations) in lines_by_fs_obj.iteritems():
-        for i in reversed(xrange(len(operations))):
-            update_dependency(operations[i], operations[:i])
+        last_update_op = None
+        for operation in operations:
+            clean_call = operation.clean_call
+            if last_update_op:
+                join(last_update_op, operation)
+            if write_semantics(clean_call.call):
+                last_update_op = operation
 
     session_counter = -1
     for (pidfid, operations) in pid_fd_sessions.iteritems():
@@ -352,14 +272,15 @@ def weak_fs_dependency_sort(workflow_lines):
     #CLOSE CASE: we keep operations sessions (from open to close).
     #we make close's parent its antecessor on session
     for (pidfid, operations) in pid_fd_sessions.iteritems():
-        for i in reversed(xrange(len(operations))):
-            current_wline = operations[i]
-            if (current_wline.clean_call.call == "close"):
+        previous_op = None
+        for operation in operations:
+            if (operation.clean_call.call == "close"):
                #assuming a close cannot be the 0th on operations list
                #this code works
-               parent_candidate = operations[i - 1]
-               if not parent_candidate.clean_call.call == "close":
-                   join(parent_candidate, current_wline)
+               if previous_op:
+                   if not previous_op.clean_call.call == "close":
+                       join(previous_op, operation)
+            previous_op = operation
 
 def conservative_sort(w_lines):
     #I guess cleaned calls are timestamp sorted TODO: check
